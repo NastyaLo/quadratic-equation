b47289aa1d6694884bd34abe9125614c6942b8c3
NastyaLo <loyko2802@gmail.com>
2018-09-13 16:49:23 +0300

    Solving Quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.replace(/\s/g, '');
+    let iA = equation.indexOf("x^2");
+    let a = equation.substring(0, iA-1);
+    let iB = equation.indexOf("x", iA+2);
+    let b = equation.substring(iA+3, iB-1);
+    let c = equation.substring(iB+1);
+
+    let discriminant = Math.round(b*b - 4*a*c);
+    let root1 = Math.round( ( -b+Math.sqrt(discriminant)) / (2*a) );
+    let root2 = Math.round( ( -b-Math.sqrt(discriminant)) / (2*a) );
+
+    return ([ root1, root2 ]).sort(( a, b ) => a - b);
 }
